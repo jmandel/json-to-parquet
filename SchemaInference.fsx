91564b6c1545dc499713a4af46805f86cdcb940e
Josh Mandel <jmandel@alum.mit.edu>
2023-03-25 12:47:52 -0500

    Handle optionality inference

--- SchemaInference.fsx
+++ SchemaInference.fsx
@@ -2,9 +2,7 @@
 
 open System.Text.Json
 // open Parquet
-
-
-type InferredSchema =
+type SchemaType =
     | JsonObject of InferredKVs
     | JsonArray of InferredElts
     | JsonNumber
@@ -15,32 +13,15 @@ type InferredSchema =
 
 and InferredKVs = Map<string, InferredSchema>
 and InferredElts = InferredSchema
-// Creating a JsonObject with an empty set of InferredKVs
-
-let emptyJsonObject = JsonObject(Map.empty)
-
-let m = Map([ "name", JsonString ])
-
-// Adding a JsonString property to the JsonObject
-let jsonObjectWithAString = JsonObject(m)
-
-// Creating a JsonArray containing JsonNumber elements
-let jsonArrayWithNumbers = JsonArray(JsonNumber)
-
-// Creating a JsonObject containing a JsonArray
-let jsonObjectWithArray = JsonObject(Map([ "numbers", jsonArrayWithNumbers ]))
-
-// Creating a JsonObject containing another JsonObject
-let innerJsonObject = JsonObject(Map([ "ageInYears", JsonNumber ]))
-let jsonObjectWithNestedObject = JsonObject(Map([ "stats", innerJsonObject ]))
-
-// Function to recursively print an InferredSchema
+and InferredSchema = { Type: SchemaType; Optional: bool }
 
 // Function to recursively print an InferredSchema with depth-based indentation
 let rec printInferredSchema (schema: InferredSchema) (depth: int) =
     let indent = String.replicate depth "  "
 
-    match schema with
+    printfn $"{indent}Optional? {schema.Optional}"
+
+    match schema.Type with
     | JsonObject kvs ->
         kvs
         |> Map.iter (fun key value ->
@@ -55,75 +36,83 @@ let rec printInferredSchema (schema: InferredSchema) (depth: int) =
     | JsonString -> printfn "%sJsonString" indent
     | _ -> printfn "%sOther" indent
 
-
-open System.Text.Json
-
-// Incrementally create an InferredSchema for a given JsonElement
 let rec createInferredSchema (element: JsonElement) : InferredSchema =
-    match element.ValueKind with
-    | JsonValueKind.Object ->
-        let kvs =
-            element.EnumerateObject()
-            |> Seq.fold
-                (fun map prop ->
-                    let propName = prop.Name
-                    let propValue = prop.Value
-                    let inferredSchema = createInferredSchema propValue
+    let schemaType =
+        match element.ValueKind with
+        | JsonValueKind.Object ->
+            let kvs =
+                element.EnumerateObject()
+                |> Seq.fold
+                    (fun map prop ->
+                        let propName = prop.Name
+                        let propValue = prop.Value
+                        let inferredSchema = createInferredSchema propValue
 
-                    map
-                    |> Map.tryFind propName
-                    |> function
-                        | Some existingSchema ->
-                            let mergedSchema = mergeInferredSchema existingSchema inferredSchema
-                            Map.add propName mergedSchema map
-                        | None -> Map.add propName inferredSchema map)
-                Map.empty
+                        map
+                        |> Map.tryFind propName
+                        |> function
+                            | Some existingSchema ->
+                                let mergedSchema = mergeInferredSchema existingSchema inferredSchema
+                                Map.add propName mergedSchema map
+                            | None -> Map.add propName inferredSchema map)
+                    Map.empty
 
-        JsonObject kvs
-    | JsonValueKind.Array ->
-        let inferredElts =
-            element.EnumerateArray()
-            |> Seq.fold
-                (fun currentSchema elem ->
-                    let elemSchema = createInferredSchema elem
-                    mergeInferredSchema currentSchema elemSchema)
-                JsonUnknown
+            JsonObject kvs
+        | JsonValueKind.Array ->
+            let inferredElts =
+                element.EnumerateArray()
+                |> Seq.fold
+                    (fun currentSchema elem ->
+                        let elemSchema = createInferredSchema elem
+                        mergeInferredSchema currentSchema elemSchema)
+                    { Type = JsonUnknown; Optional = false }
 
-        JsonArray inferredElts
-    | JsonValueKind.Number -> JsonNumber
-    | JsonValueKind.String -> JsonString
-    | JsonValueKind.True
-    | JsonValueKind.False -> JsonBoolean
-    | _ -> JsonNull
+            JsonArray inferredElts
+        | JsonValueKind.Number -> JsonNumber
+        | JsonValueKind.String -> JsonString
+        | JsonValueKind.True
+        | JsonValueKind.False -> JsonBoolean
+        | _ -> JsonNull
+
+    { Type = schemaType; Optional = false }
 
 
-// Merge two InferredSchema instances
 and mergeInferredSchema (schema1: InferredSchema) (schema2: InferredSchema) : InferredSchema =
-    match schema1, schema2 with
-    | JsonObject kvs1, JsonObject kvs2 ->
-        let mergedKvs =
-            kvs1
-            |> Map.fold
-                (fun map key value ->
-                    match kvs2 |> Map.tryFind key with
-                    | Some value2 ->
-                        let mergedValue = mergeInferredSchema value value2
-                        Map.add key mergedValue map
-                    | None -> Map.add key value map)
-                kvs2
+    let mergedType, mergedOptional =
+        match schema1.Type, schema2.Type with
+        | JsonObject kvs1, JsonObject kvs2 ->
+            let mergedKvs =
+                kvs1
+                |> Map.fold
+                    (fun map key value ->
+                        match map |> Map.tryFind key with
+                        | Some value2 ->
+                            let mergedValue = mergeInferredSchema value value2
+                            Map.add key mergedValue map
+                        | None -> Map.add key value map)
+                    kvs2
+                |> Map.fold
+                    (fun map key value ->
+                        match kvs1.ContainsKey key && kvs2.ContainsKey key with
+                        | true -> Map.add key value map
+                        | false -> Map.add key { Optional = true; Type = value.Type } map)
+                    Map.empty
 
-        JsonObject mergedKvs
-    | JsonArray elts1, JsonArray elts2 ->
-        let mergedElts = mergeInferredSchema elts1 elts2
-        JsonArray mergedElts
-    | JsonUnknown, other -> other
-    | other, JsonUnknown -> other
-    | _ when schema1 = schema2 -> schema1
-    | _ -> failwith (sprintf "Incompatible InferredSchema: %A and %A" schema1 schema2)
+            JsonObject mergedKvs, schema1.Optional || schema2.Optional
+        | JsonArray elts1, JsonArray elts2 ->
+            let mergedElts = mergeInferredSchema elts1 elts2
+            JsonArray mergedElts, schema1.Optional || schema2.Optional
+        | JsonUnknown, other -> other, schema2.Optional
+        | other, JsonUnknown -> other, schema1.Optional
+        | _ when schema1.Type = JsonNull -> schema2.Type, true
+        | _ when schema2.Type = JsonNull -> schema1.Type, true
+        | _ when schema1.Type = schema2.Type -> schema1.Type, schema1.Optional || schema2.Optional
+        | _ -> failwith (sprintf "Incompatible InferredSchema: %A and %A" schema1 schema2)
 
+    { Type = mergedType
+      Optional = mergedOptional }
 
-let jsonString =
-    """
+let jsonString = """
 [{
     "Students": [
         {
@@ -154,6 +143,9 @@ let jsonString =
             "Name": "Charlie"
         }
     ]
+}, {
+    "Teachers": true,
+    "Students": null
 }]
 """
 
